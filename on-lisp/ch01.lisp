fb72b793f75ae6b9201726f57c83d1815c1209b8
rongyi <sqrongyi@163.com>
2016-11-12 14:26:43 +0800

    Update ch01

--- on-lisp/ch01.lisp
+++ on-lisp/ch01.lisp
@@ -17,3 +17,34 @@
 (symbol-value 'double)
 ;; to get a var from function namespace
 (symbol-function 'double)
+
+(setq x #'append)
+(eq (symbol-value 'x)
+    (symbol-function 'append))
+
+;; forget what does append do
+;; (append '(a b) '(b c))
+
+
+;; apply need a list
+;; (+ 1 2)
+;; (apply '+ '(1 2))
+;; funcall need scattered element
+;; (funcall '+ 1 2 3)
+
+(apply (symbol-function '+) '(1 2))
+(apply #'(lambda (x y)
+           (+ x y))
+       '(1 2))
+
+(apply #'+ 1 '(2))
+
+;; mapcar's first arg is fn
+(mapcar #'(lambda (x)
+            (- x 10))
+        '(1 2 3))
+
+;; get the min size of two list
+(mapcar #'+
+        '(1 2 3)
+        '(10 20 30 40))
