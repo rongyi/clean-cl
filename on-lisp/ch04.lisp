3dbc6d7309e94d917c31830bcdf5b529d7592f21
rongyi <sqrongyi@163.com>
2016-11-15 16:32:02 +0800

    Update ch04

--- on-lisp/ch04.lisp
+++ on-lisp/ch04.lisp
@@ -59,3 +59,26 @@
 ;; (nthcdr 2 '(a b c))
 ;; (subseq '(a b c) 0 2)
 ;; (group '(a b c d e) 2)
+
+(defun flattern (x)
+  (labels ((rec (x acc)
+             (cond ((null x) acc)
+                   ((atom x) (cons x acc))
+                   (t (rec (car x) (rec (cdr x) acc))))))
+    (rec x nil)))
+
+;; (flattern '(a b c (d e)))
+
+(defun prune (test tree)
+  (labels ((rec (tree acc)
+             (cond ((null tree) (nreverse acc))
+                   ((consp (car tree))
+                    (rec (cdr tree)
+                         (cons (rec (car tree) nil) acc)))
+                   (t (rec (cdr tree)
+                           (if (funcall test (car tree))
+                               acc
+                               (cons (car tree) acc)))))))
+    (rec tree nil)))
+
+(prune #'evenp '(1 2 (3 (4 5) 6) 7 8 (9)))
