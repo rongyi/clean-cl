a213bd3f4478311b79929f7709aab09109c005f1
rongyi <sqrongyi@163.com>
2016-11-15 15:52:53 +0800

    Update ch12

--- ansi-cl/ch12.lisp
+++ ansi-cl/ch12.lisp
@@ -18,3 +18,22 @@
       tr
       (cons (ry/copy-tree (car tr))
             (ry/copy-tree (cdr tr)))))
+
+(defun make-queue ()
+  (cons nil nil))
+
+(defun enqueue (obj q)
+  (if (null (car q))
+      (setf (cdr q) (setf (car q) (list obj)))
+      (setf (cdr (cdr q)) (list obj)
+            (cdr q) (cdr (cdr q))))
+  (car q))
+
+(defun dequeue (q)
+  (pop (car q)))
+
+(setf q1 (make-queue))
+(enqueue 'a q1)
+(enqueue 'b q1)
+(enqueue 'c q1)
+(dequeue q1)
