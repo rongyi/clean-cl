d72df5157f82c31135ed2650ff4688b88fff5cad
rongyi <sqrongyi@163.com>
2016-11-15 16:15:50 +0800

    Update ch14

--- on-lisp/ch04.lisp
+++ on-lisp/ch04.lisp
@@ -19,3 +19,43 @@
   (if (listp obj)
       obj
       (list obj)))
+
+(defun longer (x y)
+  (labels ((cmp (x y)
+             (and (consp x)
+                  (or (null y)
+                      (cmp (cdr x) (cdr y))))))
+    (if (and (listp x) (listp y))
+        (cmp x y)
+        (> (length x)
+           (length y)))))
+
+;; (longer '(a b c e e e e e e e e e e e) '(e e e e e))
+
+(defun filter (fn lst)
+  (let ((acc nil))
+    (dolist (x lst)
+      (let ((val (funcall fn x)))
+        (if val
+            (push x acc))))             ;push original value or the funcalled value?
+    (nreverse acc)))
+
+;; (filter #'evenp '(1 2 3 4))
+
+(defun group (source n)
+  (if (zerop n)
+      (error "zero length"))
+  (labels ((rec (source acc)
+             (let ((rest (nthcdr n source)))
+               (if (consp rest)
+                   (rec rest (cons (subseq source 0 n) acc))
+                   (nreverse (cons source acc))))))
+    (if source
+        (rec source nil)
+        nil)))
+
+;; (nthcdr 0 '(a b c))
+;; (nthcdr 1 '(a b c))
+;; (nthcdr 2 '(a b c))
+;; (subseq '(a b c) 0 2)
+;; (group '(a b c d e) 2)
