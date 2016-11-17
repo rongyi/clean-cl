290f7c758b978b2adcc34133fd1091b6a3313311
rongyi <sqrongyi@163.com>
2016-11-17 09:52:45 +0800

    Update ch05

--- on-lisp/ch05.lisp
+++ on-lisp/ch05.lisp
@@ -7,3 +7,31 @@
 ;;            '(1 2 3 4 5 6))
 
 ;; shchme convention of appending ! to the names of functions with side-effects.
+
+(defvar *!equivs* (make-hash-table))
+
+(defun ry/! (fn)
+  (or (gethash fn *!equivs*)
+      fn))
+
+(defun ry/def! (fn fn!)
+  (setf (gethash fn *!equivs*)
+        fn!))
+
+(defun ry/memoize (fn)
+  (let ((cache (make-hash-table :test #'equal)))
+    #'(lambda (&rest args)
+        (multiple-value-bind (val win) (gethash args cache)
+          (if win
+              val
+              (setf (gethash args cache)
+                    (apply fn args)))))))
+
+(setq slowid (ry/memoize #'(lambda (x)
+                             (sleep 5)
+                             x)))
+
+;; no cache
+(time (funcall slowid 1))
+;; cached id 1
+(time (funcall slowid 1))
