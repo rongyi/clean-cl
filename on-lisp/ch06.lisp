4df9e77d5b296160c3924681603b736b3923e23d
rongyi <sqrongyi@163.com>
2016-11-20 11:53:08 +0800

    Update ch06

--- on-lisp/ch06.lisp
+++ on-lisp/ch06.lisp
@@ -1,12 +1,23 @@
 (defstruct node contents yes no)
-(defvar *node* (make-hash-table))
+(defvar *nodes* (make-hash-table))
 (defun defnode (name conts &optional yes no)
-  (setf (gethash name *node*)
+  (setf (gethash name *nodes*)
         (make-node :contents conts
                    :yes yes
                    :no no)))
 
-(defnode 'people "Is the person a man?" 'mail 'femail)
+(defnode 'people "Is the person a man?" 'male 'female)
 (defnode 'male "Is he living?" 'liveman 'deadman)
 (defnode 'deadman "Was he American?" 'us 'them)
 (defnode 'us "Is he on a coin?" 'coin 'cidence)
+(defnode 'coin "Is the coin a penny?" 'penny 'coins)
+(defnode 'penny 'lincoln)
+
+(defun run-node (name)
+  (let ((n (gethash name *nodes*)))
+    (cond ((node-yes n)
+           (format t "~A~%>> " (node-contents n))
+           (case (read)
+             (yes (run-node (node-yes n)))
+             (t (run-node (node-no n)))))
+          (t (node-contents n)))))
