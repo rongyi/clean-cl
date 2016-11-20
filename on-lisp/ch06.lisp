eece098bdf9a9dfb5ada4d178873f8dfbc0ba3d7
rongyi <sqrongyi@163.com>
2016-11-20 20:51:09 +0800

    Finish ch06

--- on-lisp/ch06.lisp
+++ on-lisp/ch06.lisp
@@ -45,3 +45,29 @@
 (defun defnode (&rest args)
   (push args *nodes*)
   args)
+
+(defnode 'people "Is the person a man?" 'male 'female)
+(defnode 'male "Is he living?" 'liveman 'deadman)
+(defnode 'deadman "Was he American?" 'us 'them)
+(defnode 'us "Is he on a coin?" 'coin 'cidence)
+(defnode 'coin "Is the coin a penny?" 'penny 'coins)
+(defnode 'penny 'lincoln)
+
+(defun compile-net (root)
+  (let ((node (assoc root *nodes*)))
+    (if (null node)
+        nil
+        (let ((conts (second node))
+              (yes (third node))
+              (no (fourth node)))
+          (if yes
+              (let ((yes-fn (compile-net yes))
+                    (no-fn (compile-net no)))
+                #'(lambda ()
+                    (format t "~A~%>> " conts)
+                    (funcall (if (eq (read) 'yes)
+                                 yes-fn
+                                 no-fn))))
+              #'(lambda () conts))))))
+(setq n (compile-net 'people))
+(funcall n)
