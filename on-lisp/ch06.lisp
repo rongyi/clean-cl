d7d7261a18c72ad0500d7da5fe629f3fb093fb01
rongyi <sqrongyi@163.com>
2016-11-20 12:11:45 +0800

    Update ch06

--- on-lisp/ch06.lisp
+++ on-lisp/ch06.lisp
@@ -21,3 +21,21 @@
              (yes (run-node (node-yes n)))
              (t (run-node (node-no n)))))
           (t (node-contents n)))))
+
+;; this version is a closure version
+(defun defnodev2 (name conts &optional yes no)
+  (setf (gethash name *nodes*)
+        (if yes
+            #'(lambda ()
+                (format t "~A~%>> " conts)
+                (case (read)
+                  (yes (funcall (gethash yes *nodes*)))
+                  (t (funcall (gethash no *nodes*)))))
+            #'(lambda () conts))))
+
+(defnodev2 'people "Is the person a man?" 'male 'female)
+(defnodev2 'male "Is he living?" 'liveman 'deadman)
+(defnodev2 'deadman "Was he American?" 'us 'them)
+(defnodev2 'us "Is he on a coin?" 'coin 'cidence)
+(defnodev2 'coin "Is the coin a penny?" 'penny 'coins)
+(defnodev2 'penny 'lincoln)
