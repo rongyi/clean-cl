fc6a450592cc10766ba721557dcb29bef2a4d7f2
rongyi <sqrongyi@163.com>
2016-11-21 11:34:35 +0800

    Update ch07

--- on-lisp/ch07.lisp
+++ on-lisp/ch07.lisp
@@ -4,3 +4,7 @@
 (let ((n 10))
   (nil! n))
 (macroexpand-1 '(nil! n))
+
+;; Here's the stroke of genius: the output of the parse consists of
+;; lists of Lisp Objects. With macros, we can manipulate the program
+;; while it's in this intermediate from between parse and compiler.
