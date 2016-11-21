57a7e5c00cd0ace8c53c46707d18b1811b6ab693
rongyi <sqrongyi@163.com>
2016-11-21 12:18:02 +0800

    Add ch07

--- on-lisp/ch07.lisp
+++ on-lisp/ch07.lisp
@@ -8,3 +8,29 @@
 ;; Here's the stroke of genius: the output of the parse consists of
 ;; lists of Lisp Objects. With macros, we can manipulate the program
 ;; while it's in this intermediate from between parse and compiler.
+
+;; backquote section
+(setq a 1 b 2 c 3)
+`(a ,b c)
+
+;; one comma counteracts the effect of one backquote
+;; so commas must match backquote.
+;; The general rule is: a comma surrounded by n commas
+;; must be surrounded by at least n + 1 backquotes.
+
+(defmacro nil! (var)
+  `(setq ,var nil))
+
+;; withbackquote
+(defmacro nif (expr pos zero neg)
+  `(case (truncate (signum ,expt))
+     (1 ,pos)
+     (0 ,zero)
+     (-1 ,neg)))
+;; without backquote
+(defmacro nil (expr pos zero neg)
+  (list 'case
+        (list 'truncate (list 'signum expt))
+        (list 1 pos)
+        (list 0 zero)
+        (list -1 neg)))
