21874f3abc8255ce069bcb31fe472eb81f7aa01a
rongyi <sqrongyi@163.com>
2016-12-13 10:58:06 +0800

    Update ch13

--- on-lisp/ch13.lisp
+++ on-lisp/ch13.lisp
@@ -27,3 +27,48 @@
                      args)))))
 
 ;; (most-of t t t nil)
+;; (testmacro (most-of t t t nil))
+
+;; under this situation macro is far better than function
+(defun nthmost (n lst)
+  (nth n (sort (copy-list lst) #'>)))
+
+;; (nthmost 2 '(2 6 1 5 3 4))
+(defmacro nthmost (n lst)
+  (if (and (integerp n) (< n 20))
+      (ry/with-gensyms (glst gi)
+        (let ((syms (map0-n #'(lambda (x)
+                                (gensym))
+                            n)))
+          `(let ((,glst ,lst))
+             (unless (< (length ,glst) ,(1+ n))
+               ,@(gen-start glst syms)
+               (dolist (,gi ,glst)
+                 ,(nthmost-gen gi syms t))
+               ,(car (last syms))))))
+      `(nth ,n (sort (copy-list ,lst) #'>))))
+
+(nthmost 2 '(2 6 1 5 3 4))
+
+;; (testmacro (nthmost 2 '(2 6 1 5 3 4)))
+
+(defun gen-start (glst syms)
+  (reverse
+   (maplist #'(lambda (syms)
+                (let ((var (gensym)))
+                  `(let ((,var (pop ,glst)))
+                     ,(nthmost-gen var (reverse syms)))))
+            (reverse syms))))
+
+(defun nthmost-gen (var vars &optional long?)
+  (if (null vars)
+      nil
+      (let ((else (nthmost-gen var (cdr vars) long?)))
+        (if (and (not long?) (null else))
+            `(setq ,(car vars) ,var)
+            `(if (> ,var ,(car vars))
+                 (setq ,@(mapcan #'list
+                                 (reverse vars)
+                                 (cdr (reverse vars)))
+                       ,(car vars) ,var)
+                 ,else)))))
