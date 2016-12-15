cf40cb7f70b3e4880ba304cbac6b753d7be7d5da
rongyi <sqrongyi@163.com>
2016-12-15 15:51:50 +0800

    Finish ch16

--- on-lisp/ch16.lisp
+++ on-lisp/ch16.lisp
@@ -68,3 +68,52 @@
       `(list ,@syms)))
 
 ;; (alist 1 (+ 2 it) (+ 2 it))
+
+(defmacro defanaph (name &optional calls)
+  (let ((calls (or calls (pop-symbol name))))
+    `(defmacro ,name (&rest args)
+       (anaphex args (list ',calls)))))
+
+(defun anaphex (args expr)
+  (if args
+      (let ((sym (gensym)))
+        `(let* ((,sym ,(car args))
+                (it ,sym))
+           ,(anaphex (cdr args)
+                     (append expr (list sym)))))
+      expr))
+
+(defun pop-symbol (sym)
+  (intern (subseq (symbol-name sym) 1)))
+
+;; (pop-symbol 'hell)
+
+;; (defanaph a+)
+;; (defanaph alist)
+
+(defmacro defanaph (name &optional &key calls (rule :all))
+  (let* ((opname (or calls (pop-symbol name)))
+         (body (case rule
+                 (:all `(anaphex1 args '(,opname)))
+                 (:first `(anaphex2 ',opname args))
+                 (:place `(anaphex3 ',opname args)))))
+    `(defmacro ,name (&rest args)
+       ,body)))
+
+(defun anaphex1 (args call)
+  (if args
+      (let ((sym (gensym)))
+        `(let* ((,sym ,(car args))
+                (it ,sym))
+           ,(anaphex1 (cdr args)
+                      (append call (list sym)))))
+      call))
+
+(defun anaphex2 (op args)
+  `(let ((it ,(car args)))
+     (,op it ,@(cdr args))))
+
+(defun anaphex3 (op args)
+  `(_f (lambda (it)
+         (,op it ,@(cdr args)))
+       ,(car args)))
