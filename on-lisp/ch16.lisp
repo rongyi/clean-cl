3a2face1ab502a8b654def384fabf26d326e3bd7
rongyi <sqrongyi@163.com>
2016-12-15 15:29:15 +0800

    Update ch16

--- on-lisp/ch16.lisp
+++ on-lisp/ch16.lisp
@@ -25,3 +25,46 @@
 
 ;; (abbrevs mvbind multiple-value-bind
 ;;          mvsetq multiple-value-setq)
+
+(defmacro propmacro (propname)
+  `(defmacro ,propname (obj)
+     `(get ,obj ',',propname)))
+
+(defmacro propmacros (&rest props)
+  `(progn
+     ,@(mapcar #'(lambda (p)
+                   `(propmacro ,p))
+               props)))
+
+(defmacro a+ (&rest args)
+  (a+expand args nil))
+
+(defun a+expand (args syms)
+  (if args
+      (let ((sym (gensym)))
+        `(let* ((,sym ,(car args))
+                (it ,sym))
+           ,(a+expand (cdr args)
+                      (append syms (list sym)))))
+      `(+ ,@syms)))
+
+;; (testmacro (a+ 7 (* it 100)))
+;; (a+ 7 (* it 100))
+
+;; (defun mass-cost (menu-price)
+;;   (a+ menu-price (* it .05) (* it 3)))
+;; (mass-cost 7.95)
+
+(defmacro alist (&rest args)
+  (alist-expand args nil))
+
+(defun alist-expand (args syms)
+  (if args
+      (let ((sym (gensym)))
+        `(let* ((,sym ,(car args))
+                (it ,sym))
+           ,(alist-expand (cdr args)
+                          (append syms (list sym)))))
+      `(list ,@syms)))
+
+;; (alist 1 (+ 2 it) (+ 2 it))
