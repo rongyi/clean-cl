44e6f292e2b7e4fc643d6d42c3e46514d14ea05e
rongyi <sqrongyi@163.com>
2016-12-15 10:05:06 +0800

    Update ch16

--- on-lisp/ch16.lisp
+++ on-lisp/ch16.lisp
@@ -1,1 +1,27 @@
 ;; macro define macro
+;; Fortunately, in a language like Lisp you don't have to live with all the decisions of the designers.
+
+
+;; (defmacro dbind (&rest args)
+;;   `(destructuring-bind ,@args))
+
+;; (defmacro mvbind (&rest args)
+;;   `(multiple-value-bind ,@args))
+
+;; Nested backquotes are notoriously hard to understand
+;; 嵌套backquote太难懂了
+
+(defmacro abbrev (short long)
+  `(defmacro ,short (&rest args)
+     `(,',long ,@args)))
+
+;; (testmacro (abbrev mvbind multiple-value-bind))
+
+(defmacro abbrevs (&rest names)
+  `(progn
+     ,@(mapcar #'(lambda (pair)
+                   `(abbrev ,@pair))
+               (group names 2))))
+
+;; (abbrevs mvbind multiple-value-bind
+;;          mvsetq multiple-value-setq)
