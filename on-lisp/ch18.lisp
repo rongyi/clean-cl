32db199aecef3d079f2ae9e10e360da8f5de4330
rongyi <sqrongyi@163.com>
2016-12-16 14:35:18 +0800

    Update ch18

--- on-lisp/ch18.lisp
+++ on-lisp/ch18.lisp
@@ -39,3 +39,36 @@
 
 ;; (destruc '(a (b . c) &rest d) 'seq)
 ;; (dbind-ex (destruc '(a (b . c) &rest d) 'seq) '(body))
+
+(defmacro with-matrix (pats ar &body body)
+  (let ((gar (gensym)))
+    `(let ((,gar ,ar))
+       (let ,(let ((row -1))
+               (mapcan #'(lambda (pat)
+                           (incf row)
+                           (setq col -1)
+                           (mapcar #'(lambda (p)
+                                       `(,p (aref ,gar
+                                                  ,row
+                                                  ,(incf col))))
+                                   pat))
+                       pats))
+         ,@body))))
+
+(defmacro with-array (pat ar &body body)
+  (let ((gar (gensym)))
+    `(let ((,gar ,ar))
+       (let ,(mapcar #'(lambda (p)
+                         `(,(car p) (aref ,gar ,@(cdr p))))
+                     pat)
+         ,@body))))
+
+;; (setq ar (make-array '(3 3)))
+;; (ry/for (r 0 2)
+;;      (ry/for (c 0 2)
+;;           (setf (aref ar r c) (+ (* r 10) c))))
+
+;; (with-matrix ((a b c)
+;;               (d e f)
+;;               (g h i)) ar
+;;   (list a b c d e f g h i))
