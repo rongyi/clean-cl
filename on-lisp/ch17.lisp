478774d07d282ce60796e3e29d8a2c168f78abb1
rongyi <sqrongyi@163.com>
2016-12-16 09:33:08 +0800

    Add ch17

--- on-lisp/ch17.lisp
+++ on-lisp/ch17.lisp
@@ -1,2 +1,4 @@
 ;; The three big moments in a Lisp expression's life are:
 ;; read-time compile-time and runtime
+
+;; The reader can be programmed at several levels. The easiest way to change its behavior is by defining new macro characters.
