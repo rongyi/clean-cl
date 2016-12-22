2b9c4783bceb00a013e663b64cbe4e3f04e6b96c
rongyi <sqrongyi@163.com>
2016-12-22 09:33:18 +0800

    Add ch20

---
+++ on-lisp/ch20.lisp
@@ -0,0 +1,4 @@
+;; continuation
+;; A continuation is a program frozen in action: a single functional object containing
+;; the state of a computation. When the object is evaluated, the stored computation is
+;; restarted where it left off.
