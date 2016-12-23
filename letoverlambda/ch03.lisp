03ac602a7d8352b6eb1fb361e0beef0528537935
rongyi <sqrongyi@163.com>
2016-12-23 11:09:32 +0800

    Update letoverlambda

--- letoverlambda/ch03.lisp
+++ letoverlambda/ch03.lisp
@@ -138,3 +138,14 @@
 (let ((let '`(let ((let ',let))
                ,let)))
   `(let ((let ',let)) ,let))
+
+(defun |#"-reader| (stream sub-char numarg)
+  (declare (ignore sub-char numarg))
+  (let ((chars nil))
+    (do ((prev (read-char stream) curr)
+         (curr (read-char stream) (read-char stream)))
+        ((and (char= prev #\") (char= curr #\#)))
+      (push prev chars))
+    (coerce (nreverse chars) 'string)))
+
+(set-dispatch-macro-character #\# #\" #'|#"-reader|)
