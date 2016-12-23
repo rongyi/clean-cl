0c028628cc48e65ed1a6b81210403891a9bc220d
rongyi <sqrongyi@163.com>
2016-12-23 09:30:46 +0800

    Update letoverlambda

--- letoverlambda/ch03.lisp
+++ letoverlambda/ch03.lisp
@@ -1,3 +1,39 @@
+;; 这个函数只是在一个stream中找到target，找到一次之后就结束了，
+;; 后面出现不出现已经不重要了，因为这个函数始终会返回T， shit
+(defun block-scanner (trigger-string)
+  (let* ((trig (coerce trigger-string 'list))
+         (curr trig))
+    (lambda (data-string)
+      (let ((data (coerce data-string 'list))
+            (ret nil))
+        (dolist (c data)
+          (if curr
+              (setq curr
+                    (if (char= (car curr) c)
+                        (cdr curr)
+                        trig))))
+        (not curr)))))
+
+;; (defvar scanner (block-scanner "jihad"))
+;; (funcall scanner "jiha")
+;; (funcall scanner "d")
+;; (funcall scanner "jiha")
+;; (funcall scanner "jihad")
+;; (funcall scanner "jihad")
+;; (funcall scanner "we will ji")
+;; (funcall scanner "had")
+;; (funcall scanner "jihad")
+;; (funcall scanner "jihajihad")
+
+
+(defun flatten (x)
+  (labels ((rec (x acc)
+             (cond ((null x) acc)
+                   ((atom x) (cons x acc))
+                   (t (rec (car x)
+                           (rec (cdr x) acc))))))
+    (rec x nil)))
+
 (defun g!-symbol-p (s)
   (and (symbolp s)
        (> (length (symbol-name s)) 2)
@@ -29,3 +65,6 @@
 ;; (let ((*print-circle* t))
 ;;   (print
 ;;    (macroexpand '(nif -1 'pos 'zero 'neg))))
+;; from now on all the source code will be put to one file!!!
+(defmacro testmacro (expr)
+  `(pprint (macroexpand-1 ',expr)))
--- on-lisp/ch20.lisp
+++ on-lisp/ch20.lisp
@@ -2,3 +2,10 @@
 ;; A continuation is a program frozen in action: a single functional object containing
 ;; the state of a computation. When the object is evaluated, the stored computation is
 ;; restarted where it left off.
+
+;; We can think of the continuation at any given time as a function of one argument.
+
+;; A closure is a function plus pointers to the lexical variables visible at the time it was created.
+
+;; A continuation is a function plus a pointer to the whole stack pending at the time it was created.
+;; When a continuation is evaluated, it returns a value using its own copy of the stack, ignoring the current one.
