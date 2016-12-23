9fe8bfdde74302eb4e6172d9e1d00b67aba5c74f
rongyi <sqrongyi@163.com>
2016-12-23 13:47:52 +0800

    Update ch03

--- letoverlambda/ch03.lisp
+++ letoverlambda/ch03.lisp
@@ -149,3 +149,31 @@
     (coerce (nreverse chars) 'string)))
 
 (set-dispatch-macro-character #\# #\" #'|#"-reader|)
+
+(defun |#>-reader| (stream sub-char numarg)
+  (declare (ignore sub-char numarg))
+  (let ((chars))
+    (do ((curr (read-char stream)
+               (read-char stream)))
+        ((char= #\newline curr))
+      (push curr chars))
+    (let* ((pattern (nreverse chars))
+           (pointer pattern)
+           (output))
+      (do ((curr (read-char stream)
+                 (read-char stream)))
+          ((null pointer))
+        (push curr output)
+        (setf pointer
+              (if (char= (car pointer) curr)
+                  (cdr pointer)
+                  pattern))
+        (if (null pointer)
+            (return)))
+      (coerce
+       (nreverse
+        (nthcdr (length pattern) output))
+       'string))))
+
+(set-dispatch-macro-character
+ #\# #\> #'|#>-reader|)
