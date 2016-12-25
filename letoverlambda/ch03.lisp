7570d648ff02d2deacc021023f254ff43ee26e56
rongyi <sqrongyi@163.com>
2016-12-25 11:02:49 +0800

    Update letoverlambda

--- letoverlambda/ch03.lisp
+++ letoverlambda/ch03.lisp
@@ -181,3 +181,13 @@
 
 
 ;; (nthcdr 3 '(fail another string for))
+
+(defun segment-reader (stream ch n)
+  (if (> n 0)
+      (let ((chars))
+        (do ((curr (read-char stream)
+                   (read-char stream)))
+            ((char= ch curr))
+          (push curr chars))
+        (cons (coerce (nreverse chars) 'string)
+              (segment-reader stream ch (- n 1))))))
