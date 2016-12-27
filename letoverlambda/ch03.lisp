7c179575df68fd4ca7d178564d35ed2edb82793d
rongyi <sqrongyi@163.com>
2016-12-27 09:38:42 +0800

    Update letoverlambda

--- letoverlambda/ch03.lisp
+++ letoverlambda/ch03.lisp
@@ -491,3 +491,12 @@
 ;; (count-test :bound -10 100)
 ;; (count-test :inc)
 ;; (count-test :inc 1000)
+
+(defun |#`-reader| (stream sub-char numarg)
+  (declare (ignore sub-char))
+  (unless numarg (setq numarg 1))
+  `(lambda ,(loop for i from 1 to numarg
+               collect (symb 'a i))
+     ,(funcall (get-macro-character #\`) stream nil)))
+
+(set-dispatch-macro-character #\# #\` #'|#`-reader|)
